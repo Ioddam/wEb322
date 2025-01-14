036b7b07b3c56e4034f9d835aad60d0f9211c725
Ioddam <nixenhorizon@gmail.com>
2025-01-14 15:25:13 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// setup a 'root' route to listen on the default url path (i.e. '/')
+
+app.get("/", (req, res) => {
+
+res.send("Hello World!<br /><a href= '/about' >Go to the about page</a>");
+
+});
+
+// define a route (i.e. an endpoint) to listen on the '/about' url path
+
+app.get("/about", (req, res) => {
+
+res.json({course: "WEB322", student: "Nikash", task: "In-class Assignment 1",date: new Date()});
+
+});
+
+// setup http server to listen on HTTP_PORT 
+app.listen(HTTP_PORT);
